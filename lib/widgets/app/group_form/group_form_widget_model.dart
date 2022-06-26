72eb015fcd9cf90c98e181e04fb5475cdb1acf32
Anton Mitkow <antosha.mitkov@mail.ru>
2022-06-26 20:02:15 +0600

    ToDoList create project

---
+++ lib/widgets/app/group_form/group_form_widget_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_list/domain/entity/group.dart';
+
+class GroupFormWidgetModel extends ChangeNotifier {
+  var groupName = '';
+  void saveGroup(BuildContext context) async {
+    if (groupName.isEmpty) return;
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(GroupAdapter());
+    }
+    final box = await Hive.openBox<Group>('group_box');
+    final group = Group(name: groupName);
+    await box.add(group);
+    Navigator.of(context).pop();
+  }
+}
+
+class GroupFormWidgetModelProvider extends InheritedWidget {
+  final GroupFormWidgetModel model;
+  const GroupFormWidgetModelProvider({
+    Key? key,
+    required this.model,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static GroupFormWidgetModelProvider? watch(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<GroupFormWidgetModelProvider>();
+  }
+
+  static GroupFormWidgetModelProvider? read(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<GroupFormWidgetModelProvider>()
+        ?.widget;
+    return widget is GroupFormWidgetModelProvider ? widget : null;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
+  }
+}
---
+++ lib/widgets/app/groups/groups_widget.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo_list/domain/entity/group.dart';
+import 'package:todo_list/widgets/app/groups/groups_widget_model.dart';
+
+class GroupsWidget extends StatefulWidget {
+  const GroupsWidget({Key? key}) : super(key: key);
+
+  @override
+  State<GroupsWidget> createState() => _GroupsWidgetState();
+}
+
+class _GroupsWidgetState extends State<GroupsWidget> {
+  final _model = GroupsWidgetModel();
+  @override
+  Widget build(BuildContext context) {
+    return GroupsWidgetModelProvider(
+        model: _model, child: const _GroupWidgetBody());
+  }
+}
+
+class _GroupWidgetBody extends StatelessWidget {
+  const _GroupWidgetBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Записи"),
+        centerTitle: true,
+      ),
+      body: const _GroupListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            GroupsWidgetModelProvider.read(context)?.model.showForm(context),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _GroupListWidget extends StatelessWidget {
+  const _GroupListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final groups = GroupsWidgetModelProvider.watch(context)?.model.groups ?? [];
+    return ListView.separated(
+        separatorBuilder: (BuildContext contex, int index) {
+          return const Divider(height: 1);
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return _GroupListRowWidget(
+            groups: groups[index],
+            indexInList: index,
+          );
+        },
+        itemCount: groups.length);
+  }
+}
+
+class _GroupListRowWidget extends StatelessWidget {
+  final int indexInList;
+  final Group groups;
+  const _GroupListRowWidget(
+      {Key? key, required this.groups, required this.indexInList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = GroupsWidgetModelProvider.read(context)!.model;
+    // final group = model.groups[indexInList];
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        // dismissible: DismissiblePane(
+        //     key: Key(indexInList.toString()), onDismissed: () {}),
+        children: [
+          SlidableAction(
+            onPressed: (context) => model.deleteGroup(indexInList),
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(groups.name),
+        trailing: const Icon(Icons.chevron_right),
+        onTap: () {},
+      ),
+    );
+  }
+}
